0cba243f1621d1afed8ca0cd968e80e4d3230af8
ibzimh <115051423+ibzimh@users.noreply.github.com>
2022-11-12 19:39:11 -0500

    Update ClassicalGenerator.py

--- ClassicalGenerator.py
+++ ClassicalGenerator.py
@@ -9,11 +9,14 @@ listofprimes = lst.get_list_of_primes()
 
 def clprime(n):
   pi = 4
+  
+  # product of the first few (pi) primes 
   prod = arr.reduce(listofprimes[:pi], lambda acc, elem: acc*elem, 1)
+  
+  # this takes the idea of the naive generator checking for non-evens and uses it to filter out more numbers.
+  
+  # a random number in the range which is coprime to pi
   q = r.rand_co_prime(n, pi)
-  t = 0
-  while(not r.is_prime(q) or t > 100):
+  while(not r.is_prime(q)):
     q += prod
-    t += 1
-  print("clprime trails: ", t)
   return q
