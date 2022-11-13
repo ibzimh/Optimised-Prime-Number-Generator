09b1139b8ebb557d0d7d70939fecd16dcde748ed
ibzimh <115051423+ibzimh@users.noreply.github.com>
2022-11-12 19:39:58 -0500

    Update naiveGenerator.py

--- naiveGenerator.py
+++ naiveGenerator.py
@@ -8,18 +8,13 @@ import primeHelpers as r
 
 def nprime(n):
   q = r.rand_odd(n)
-  t = 0
   while(not r.is_prime(q)):
     q = r.rand_odd(n)
-    t += 1
-  print("nprime trails: ", t)
   return q
 
+# slightly more efficent 
 def nprime2(n):
   q = r.is_prime(n)
-  t = 0
-  while(not r.is_prime(q) or t > 100):
+  while(not r.is_prime(q)):
     q += 2
-    t += 1
-  print("nprime2 trails: ", t)
   return q
